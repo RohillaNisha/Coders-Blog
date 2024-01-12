3a76ca05a7ccd568838929e7464bdb32b0d37151
Nisha <nisha.bioinfo@gmail.com>
2024-01-12 09:31:16 +0100

    starting project

---
+++ src/main/java/rasmus/nisha/codersblog/CodersBlogApplication.java
@@ -0,0 +1,13 @@
+package rasmus.nisha.codersblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodersBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodersBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rasmus/nisha/codersblog/CodersBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package rasmus.nisha.codersblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodersBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
