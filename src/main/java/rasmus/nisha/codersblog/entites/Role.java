c8cabf39bd45f8836e4afdd68154842c13591ac5
rasmusdavid <rasmusdavidsson@hotmail.com>
2024-01-12 15:18:53 +0100

    Added user entity and 2 default users in the database

---
+++ src/main/java/rasmus/nisha/codersblog/entites/Role.java
@@ -0,0 +1,8 @@
+package rasmus.nisha.codersblog.entites;
+
+public enum Role {
+
+    ROLE_USER,
+    ROLE_ADMIN
+
+}
---
+++ src/main/java/rasmus/nisha/codersblog/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package rasmus.nisha.codersblog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import rasmus.nisha.codersblog.entites.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
+}
