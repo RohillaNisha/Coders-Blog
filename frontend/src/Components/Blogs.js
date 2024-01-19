a8a75143d631b3461f4629e89cd4a8645df0d173
rasmusdavid <rasmusdavidsson@hotmail.com>
2024-01-19 13:40:50 +0100

    Made a search function.

--- frontend/src/Components/Blogs.js
+++ frontend/src/Components/Blogs.js
@@ -3,6 +3,7 @@ import React, {useEffect, useState} from "react";
 function Blogs() {
 
     const [filteredData, setFilteredData] = useState([]);
+    const [searchedValue, setSearchedValue] = useState("");
 
     useEffect(() => {
         allBlogs()
@@ -29,19 +30,46 @@ function Blogs() {
 
     console.log(filteredData);
 
+    async function handleSearch() {
+        if(searchedValue === ""){
+            await allBlogs()
+        }
+        try {
+            const res = await fetch(`http://localhost:8080/api/blog/search/${searchedValue}`, {
+                credentials: "include",
+            });
+
+            if (!res.ok) {
+                throw new Error('Http error');
+            }
+
+            const body = await res.json();
+            console.log(body);
+            setFilteredData(body);
+        } catch (error) {
+            console.error('Error fetching blogs:', error.message);
+        }
+    }
+
 
     return(
         <div>
+            <div className="input-group">
+                <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search"
+                       aria-describedby="search-addon" value={searchedValue} onChange={(event) => {setSearchedValue(event.target.value)}}/>
+                <button type="button" className="btn btn-outline-primary" onClick={handleSearch} data-mdb-ripple-init>search</button>
+            </div>
+
             {filteredData.map((element) => (
-                <div className = "card ">
-                <div key={element.blogId} className="card">
-                    <div className="card-header">{element.blogId}</div>
-                    <div className="card-body">
-                        <h5 className="card-title"><a href="#">{element.title}</a></h5>
-                        <p className="card-text">{element.content}</p>
+                <div className="card ">
+                    <div key={element.blogId} className="card">
+                        <div className="card-header">{element.blogId}</div>
+                        <div className="card-body">
+                            <h5 className="card-title"><a href="#">{element.title}</a></h5>
+                            <p className="card-text">{element.content}</p>
+                        </div>
+                        <div className="card-footer text-muted">{element.postDate}</div>
                     </div>
-                    <div className="card-footer text-muted">{element.postDate}</div>
-                </div>
                 </div>
             ))}
         </div>
